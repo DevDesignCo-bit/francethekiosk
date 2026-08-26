b12b63fadebe58ba81be406a232770f02ebe4ebe
Company <figma@infinite-plus.com>
2026-08-26 14:43:30 +0200

    Add files via upload

---
+++ data/story.js
@@ -0,0 +1,57 @@
+var story = {
+ "docName": "France-Subscription---Thekiosk---SFR,-Bouygues-Telecom.",
+ "docPath": "P_P_P",
+ "docVersion": 100000001,
+ "ownerName": "",
+ "ownerEmail": "",
+ "authorName": "V_V_N",
+ "authorEmail": "V_V_E",
+ "fileType": "png",
+ "disableInteractions": false,
+ "highlightHotspot": true,
+ "highlightAllHotspots": true,
+ "showFigmaLinks": false,
+ "hideGallery": true,
+ "galleryPageColorsEnabled": false,
+ "galleryDecorNodesEnabled": true,
+ "zoomEnabled": true,
+ "cloud": false,
+ "singleFile": false,
+ "title": "France-Subscription---Thekiosk---SFR,-Bouygues-Telecom.",
+ "layersExist": false,
+ "pages": [
+  {
+   "id": "1:5",
+   "groupIndex": 0,
+   "title": "Landing",
+   "image": "landing.png",
+   "index": 0,
+   "width": 390,
+   "height": 844,
+   "x": 320.0262756347656,
+   "y": -366,
+   "awidth": 390,
+   "aheight": 844,
+   "ax": 0,
+   "ay": 0,
+   "protoOverflowV": false,
+   "protoOverflowH": false,
+   "isFrame": true,
+   "type": "regular",
+   "fixedPanels": [],
+   "links": [],
+   "layout": null
+  }
+ ],
+ "startPageIndex": 0,
+ "totalImages": 1,
+ "groups": [
+  {
+   "id": "0:1",
+   "index": 0,
+   "name": "DEV",
+   "backColor": "#464646",
+   "sections": []
+  }
+ ]
+}
